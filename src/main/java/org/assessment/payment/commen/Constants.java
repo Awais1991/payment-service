bd1b22c56bbbb058cba5b6eb933e84a06888e77e
AwaisBinYousaf <awais.yousaf@digitify.com>
2024-02-01 03:22:44 +0500

    fee url update

--- src/main/java/org/assessment/payment/commen/Constants.java
+++ src/main/java/org/assessment/payment/commen/Constants.java
@@ -3,6 +3,6 @@ package org.assessment.payment.commen;
 public class Constants {
 
     public static final String URL_ADD_RECEIPT="/api/addReceipt";
-    public static final String URL_GET_FEES="/api/fees/grade/";
+    public static final String URL_GET_FEES="/api/fees/grades/";
     public static final String URL_GET_STUDENT="/api/students/rollNo/";
 }
